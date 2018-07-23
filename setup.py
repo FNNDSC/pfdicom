a835027b839b051321b4376054438f949fc1ed97
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-07-23 17:26:00 -0400

    Bump to 1.2.10

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdicom',
-      version          =   '1.2.8',
+      version          =   '1.2.10',
       description      =   '(Python) Base module for parsing DICOM files in the pf* family.',
       long_description =   readme(),
       author           =   'FNNDSC',
