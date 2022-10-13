e844b419069ccc27430d8407344625771948d367
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-10-13 17:33:33 -0400

    Revamp to new design pattern

--- pfdicom/__init__.py
+++ pfdicom/__init__.py
@@ -1,4 +1,4 @@
-try:
-    from .pfdicom    import pfdicom
-except:
-    from pfdicom     import pfdicom
+from importlib.metadata import Distribution
+
+__pkg       = Distribution.from_name(__package__)
+__version__ = __pkg.version
