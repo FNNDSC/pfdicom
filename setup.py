64d90372cb65b864e7c46e04ec6751fdbab4712b
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2022-10-13 18:21:36 -0400

    bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfdicom',
-      version          =   '2.0.4',
+      version          =   '2.0.6',
       description      =   'Base module for parsing DICOM files in the pf* family.',
       long_description =   readme(),
       author           =   'FNNDSC',
